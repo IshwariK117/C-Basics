ebf6adba34f64c1511cf4863a262c2a92f0c939d
https://github.com/IshwariK117 <ishwarikape2003@gmail.com>
2024-08-23 14:59:12 +0530

    added

---
+++ numberCheck.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n==0){
+        cout<<"ohh..its zero";
+    }
+    else if(n<0){
+        cout<<n<<" is negative number";
+    }
+    else{
+        cout<<n<<" is positive number";
+    }
+
+
+    return 0;
+
+}
