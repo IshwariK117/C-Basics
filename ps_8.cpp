f65ab8699fcdf35f7f95f0fae6c21f3085cb5198
https://github.com/IshwariK117 <ishwarikape2003@gmail.com>
2024-09-02 22:43:21 +0530

    added

---
+++ ps_8.cpp
@@ -0,0 +1,22 @@
+/*
+Odd Sum
+You are the class teacher of N students, and are assigning them a task in which they need to find the odd parts of all their roll numbers. The odd parts of the roll numbers can be calculated using the following formula:
+r[i]  = ( 2k ) * t (where r is roll number of the ith student, k is the nearest maximum power of 2 less than ith number and t is the odd part of the roll number)
+Your task is to find and return an integer value representing the sum of odd parts of the roll numbers of every student in the class.
+Note: Return the output modulo 10007.
+Input: An integer value N, representing the number of students.
+Output: Return an integer value representing the sum of the odd part of the roll number of every student in the class.
+Example 1:
+Input: 6
+Output: 14
+Example 2:
+Input: 2
+Output: 2
+Example 3:
+Input: 10
+Output: 36
+*/
+
+
+
+
